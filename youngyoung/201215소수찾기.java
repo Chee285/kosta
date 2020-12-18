78c3cd9866c42239bee580dbbedb9d1dc19e83a5
youngyoung <kxodud10054@gmail.com>
2020-12-18 09:58:58 +0900

    Create 201215소수찾기.java

---
+++ youngyoung/201215소수찾기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=2; i<=n; i++){
+            boolean check = true;
+            for(int j=2; j*j<=i; j++){
+                if(i%j == 0){
+                 
+                check=false;
+                break;   
+                }
+            }
+                if(check){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
