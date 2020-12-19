7ee6949900f74b10789e28ea94175d74e673da7c
MIN HO <wooilds1@koreatech.ac.kr>
2020-12-19 21:11:28 +0900

    2020.12.19

--- YOO MIN HO/LEVEL1/64061 유민호 크레인 인형뽑기 게임.java
+++ YOO MIN HO/LEVEL1/64061 유민호 크레인 인형뽑기 게임.java
@@ -10,35 +10,34 @@ class Solution {
 		Stack<Integer>[] lstack = new Stack[board.length];
         
         for(int i=0;i<lstack.length;i++) {
-			lstack[i] = new Stack<Integer>();
-		}
+		lstack[i] = new Stack<Integer>();
+	}
         
         for(int col=0;col<board[0].length;col++) { 
-			for(int row=board.length-1;row>=0;row--) {
-				if(board[row][col]!=0) {
-					lstack[col].push(board[row][col]);
-				}
+		for(int row=board.length-1;row>=0;row--) {
+			if(board[row][col]!=0) {
+				lstack[col].push(board[row][col]);
 			}
 		}
+	}
         
         for(int i=0;i<moves.length;i++) {
-			if(!lstack[moves[i]-1].isEmpty()) {
-				if(!rstack.isEmpty()) {
-					if(rstack.peek()==lstack[moves[i]-1].peek()) {
-						answer+=2;
-						rstack.pop();
-						lstack[moves[i]-1].pop();
-					}
-					else {
-						rstack.push(lstack[moves[i]-1].pop());
-					}
+		if(!lstack[moves[i]-1].isEmpty()) {
+			if(!rstack.isEmpty()) {
+				if(rstack.peek()==lstack[moves[i]-1].peek()) {
+					answer+=2;
+					rstack.pop();
+					lstack[moves[i]-1].pop();
 				}
 				else {
 					rstack.push(lstack[moves[i]-1].pop());
 				}
 			}
+			else {
+				rstack.push(lstack[moves[i]-1].pop());
+			}
 		}
-        
+	}
         return answer;
     }
 }
